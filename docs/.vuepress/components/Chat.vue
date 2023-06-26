5cc03668e704d5a2667b54a2e231eb4e4d4cb545
Nikolay Telepenin <ntelepenin@cloudlinux.com>
2023-06-26 17:02:21 +0100

    Using prod connection

--- docs/.vuepress/components/Chat.vue
+++ docs/.vuepress/components/Chat.vue
@@ -98,8 +98,8 @@ export default {
         })
 
         console.log('Starting connection...')
-        // this.connection = new WebSocket('wss://doc-bot.cloudlinux.com:2096')
-        this.connection = new WebSocket('ws://localhost:8765') // dev
+        this.connection = new WebSocket('wss://doc-bot.cloudlinux.com:2096')
+        // this.connection = new WebSocket('ws://localhost:8765') // dev
 
         this.connection.onmessage = (response) => {
             const event = JSON.parse(response.data)
