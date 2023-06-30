3487257407aa4c97c0c35a6105a2765367d72d82
Anna <alevchenko@cloudlinux.com>
2023-06-30 11:25:45 +0300

    Added feedback button
    TCR-198: add a feedback button at https://docs.tuxcare.com/tuxcare/

--- docs/.vuepress/config-client/navbarLinks.ts
+++ docs/.vuepress/config-client/navbarLinks.ts
@@ -11,5 +11,10 @@ export default [
       text: "Submit support request",
       url: "https://www.tuxcare.com/support-portal/",
       class: "btn"
+    },
+    {
+      text: "Submit your idea",
+      url: "https://features.tuxcare.com/submit-idea/",
+      class: "btn"
     }
   ]
